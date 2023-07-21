6befdb9e2ed99b0c4a5767ee09cf13e84f8bf221
so1der <iluailua13@gmail.com>
2023-07-21 11:02:59 +0300

    initial commit

---
+++ src/PT6314.cpp
@@ -0,0 +1,154 @@
+#include "PT6314.h"
+#include <inttypes.h>
+#include "Arduino.h"
+
+PT6314::PT6314(uint8_t sck, uint8_t stb, uint8_t si)
+{
+    init(sck, stb, si);
+}
+
+void PT6314::init(uint8_t sck, uint8_t stb, uint8_t si)
+{
+    _sck_pin = sck;
+    _stb_pin = stb;
+    _si_pin = si;
+
+    _displayfunction = VFD_FUNCTIONSET | VFD_8BITMODE | VFD_1LINE | VFD_BRT_100;
+}
+
+void PT6314::begin(uint8_t cols, uint8_t rows, uint8_t brt)
+{
+    _brt = brt;
+    _numlines = rows;
+    if (_numlines > 1)
+    {
+        _displayfunction |= VFD_2LINE;
+    }
+
+    switch (_brt)
+    {
+    case 0:
+        break;
+    case 1:
+        _displayfunction |= VFD_BRT_75;
+        break;
+    case 2:
+        _displayfunction |= VFD_BRT_50;
+        break;
+    case 3:
+        _displayfunction |= VFD_BRT_25;
+        break;
+    }
+
+    _displaycontrol = VFD_DISPLAYCONTROL | VFD_DISPLAYON | VFD_CURSOROFF | VFD_BLINKOFF;
+
+    _displaymode = VFD_ENTRYMODESET | VFD_INCREMENT | VFD_CURSORSHIFTENABLED;
+
+    delayMicroseconds(50000);
+
+    send(COMMAND, VFD_CLEARDISPLAY);
+    delayMicroseconds(2000);
+    send(COMMAND, _displaycontrol);
+    delayMicroseconds(4500);
+    send(COMMAND, _displaymode);
+    delayMicroseconds(4500);
+    send(COMMAND, _displayfunction);
+    delayMicroseconds(4500);
+}
+
+void PT6314::send(uint8_t mode, uint8_t value)
+{
+    int full_payload = value | (mode << 8);
+    digitalWrite(_stb_pin, 0);
+    delayMicroseconds(1);
+    digitalWrite(_sck_pin, 0);
+    for (int i = 15; i > -1; i--)
+    {
+        digitalWrite(_si_pin, bitRead(full_payload, i));
+        clockPulse();
+    }
+    digitalWrite(_sck_pin, 1);
+    digitalWrite(_stb_pin, 1);
+    delayMicroseconds(1);
+}
+
+void PT6314::clockPulse()
+{
+    delayMicroseconds(2);
+    digitalWrite(_sck_pin, 1);
+    delayMicroseconds(2);
+    digitalWrite(_sck_pin, 0);
+}
+
+void PT6314::clear()
+{
+    send(COMMAND, VFD_CLEARDISPLAY);
+    delayMicroseconds(2000);
+}
+
+void PT6314::home()
+{
+    send(COMMAND, VFD_RETURNHOME);
+}
+
+void PT6314::noDisplay()
+{
+    _displaycontrol &= ~VFD_DISPLAYON;
+    send(COMMAND, _displaycontrol);
+}
+void PT6314::display()
+{
+    _displaycontrol |= VFD_DISPLAYON;
+    send(COMMAND, _displaycontrol);
+}
+
+// Turns the underline cursor on/off
+void PT6314::noCursor()
+{
+    _displaycontrol &= ~VFD_CURSORON;
+    send(COMMAND, _displaycontrol);
+}
+void PT6314::cursor()
+{
+    _displaycontrol |= VFD_CURSORON;
+    send(COMMAND, _displaycontrol);
+}
+
+// Turn on and off the blinking cursor
+void PT6314::noBlink()
+{
+    _displaycontrol &= ~VFD_BLINKON;
+    send(COMMAND, _displaycontrol);
+}
+void PT6314::blink()
+{
+    _displaycontrol |= VFD_BLINKON;
+    send(COMMAND, _displaycontrol);
+}
+
+void PT6314::setCursor(uint8_t col, uint8_t row)
+{
+    uint8_t position = col;
+    if (row > 0)
+    {
+        position += 0x40;
+    }
+    send(COMMAND, VFD_SETDDRAMADDR | position);
+}
+
+inline size_t PT6314::write(uint8_t value)
+{
+    send(DATA, value);
+    return 1; // assume sucess
+}
+
+void PT6314::createChar(uint8_t location, uint8_t charmap[])
+{
+    location &= 0x7; // we only have 8 locations 0-7
+    send(COMMAND, VFD_SETCGRAMADDR | (location << 3));
+    for (int i = 0; i < 8; i++)
+    {
+        send(DATA, charmap[i]);
+    }
+    home();
+}
---
+++ src/PT6314.h
@@ -0,0 +1,90 @@
+#ifndef PT6314_H
+#define PT6314_H
+#include <inttypes.h>
+#include "Print.h"
+
+// modes
+#define COMMAND 0xF8
+#define DATA 0xFA
+#define CHAR_DATA 0x7D
+
+// commands
+#define VFD_CLEARDISPLAY 0x01
+#define VFD_RETURNHOME 0x02
+#define VFD_ENTRYMODESET 0x04
+#define VFD_DISPLAYCONTROL 0x08
+#define VFD_CURSORSHIFT 0x10
+#define VFD_FUNCTIONSET 0x20
+#define VFD_SETCGRAMADDR 0x40
+#define VFD_SETDDRAMADDR 0x80
+
+// flags for display entry mode
+#define VFD_DECREMENT 0x00
+#define VFD_INCREMENT 0x02
+#define VFD_DISPLAYSHIFTENABLED 0x01
+#define VFD_CURSORSHIFTENABLED 0x00
+
+// flags for display on/off control
+#define VFD_DISPLAYON 0x04
+#define VFD_DISPLAYOFF 0x00
+#define VFD_CURSORON 0x02
+#define VFD_CURSOROFF 0x00
+#define VFD_BLINKON 0x01
+#define VFD_BLINKOFF 0x00
+
+// flags for display/cursor shift
+#define VFD_DISPLAYMOVE 0x08
+#define VFD_CURSORMOVE 0x00
+#define VFD_MOVERIGHT 0x04
+#define VFD_MOVELEFT 0x00
+
+// flags for function set
+#define VFD_8BITMODE 0x10 // Irrelevant for serial data transfer
+#define VFD_4BITMODE 0x00 // Irrelevant for serial data transfer
+#define VFD_2LINE 0x08
+#define VFD_1LINE 0x00
+#define VFD_BRT_100 0x00
+#define VFD_BRT_75 0x01
+#define VFD_BRT_50 0x02
+#define VFD_BRT_25 0x03
+
+class PT6314 : public Print
+{
+public:
+    PT6314(uint8_t sck, uint8_t stb, uint8_t si);
+    void init(uint8_t sck, uint8_t stb, uint8_t si);
+    void begin(uint8_t cols, uint8_t rows, uint8_t brt = 0);
+
+    void clear();
+    void home();
+
+    void noDisplay();
+    void display();
+    void noBlink();
+    void blink();
+    void noCursor();
+    void cursor();
+    void setCursor(uint8_t, uint8_t);
+    void createChar(uint8_t, uint8_t[]);
+
+    void sendtoVFD(uint8_t);
+    virtual size_t write(uint8_t);
+
+    using Print::write;
+
+private:
+    void send(uint8_t, uint8_t);
+    void clockPulse();
+
+    uint8_t _sck_pin; // Clock pin
+    uint8_t _stb_pin; // Strobe pin
+    uint8_t _si_pin;  // Serial Input pin
+    uint8_t _brt;     // Brightness (0-3)
+
+    uint8_t _displayfunction;
+    uint8_t _displaycontrol;
+    uint8_t _displaymode;
+    uint8_t _numlines;
+};
+
+#endif
