37c155c39a8ca9636072c4d61f3af19343bad573
so1der <iluailua13@gmail.com>
2023-07-21 12:02:29 +0300

    initial commit

---
+++ examples/2_20_hello_world/2_20_hello_world.ino
@@ -0,0 +1,22 @@
+#include <PT6314.h>
+
+#define SER 11
+#define ST 8
+#define CLK 13
+
+PT6314 vfd(CLK, ST, SER);
+
+void setup()
+{
+  pinMode(SER, OUTPUT);
+  pinMode(ST, OUTPUT);
+  pinMode(CLK, OUTPUT);
+  vfd.begin(20, 2, 0);
+  vfd.home();
+  vfd.print("Hello World");
+}
+
+
+void loop()
+{
+}
